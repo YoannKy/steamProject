ff8ffbaebe8ec8e6838cf1a7a5f5dca35c0bd9db
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 17:20:05 +0200

    display static game data

--- app/components/details/details.ts
+++ app/components/details/details.ts
@@ -1,12 +1,25 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 
 @Component({
   selector: 'details'
 })
 @View({
-  templateUrl: 'components/details/details.html'
+  templateUrl: 'components/details/details.html',
+  directives: [NgFor]
 })
 
 export class Details {
+  gameDetails: Array<Object>;
 
+  constructor() {
+    this.gameDetails = [
+      {
+        title: 'Counter-Strike: Global Offensive',
+        owners: 14469466,
+        playersForever: 13908311
+      }
+    ];
+  }
 }
+
+bootstrap(Details);
