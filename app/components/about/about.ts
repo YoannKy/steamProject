0d8c13099b05f45d67f7574f0df49ff9267347e9
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 13:20:19 +0200

    add home and about components

---
+++ app/components/about/about.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'about'
+})
+@View({
+  templateUrl: 'components/about/about.html',
+})
+
+export class About {
+
+}
---
+++ app/components/home/home.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'homepage'
+})
+@View({
+  templateUrl: 'components/home/home.html',
+})
+
+export class Home {
+
+}
