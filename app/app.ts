938a311027fa1500ad50e2f22c174ab62ce5a6ee
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 16:35:53 +0200

    add details component

--- app/app.ts
+++ app/app.ts
@@ -4,13 +4,15 @@ import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2
 
 import {Home} from 'components/home/home';
 import {About} from 'components/about/about';
+import {Details} from 'components/details/details';
 
 @Component({
   selector: 'app'
 })
 @RouteConfig([
   { path: '/', component: Home, as: 'home' },
-  { path: '/about', component: About, as: 'about' }
+  { path: '/about', component: About, as: 'about' },
+  { path: '/details/', component: Details, as: 'details' }
 ])
 @View({
   templateUrl: 'app.html',
