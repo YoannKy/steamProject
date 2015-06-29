2e9c27d0c7b1ecb30de7e6afc48b01b35fa28d5a
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 18:45:30 +0200

    add features component

---
+++ app/components/features/features.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'features'
+})
+@View({
+  templateUrl: 'components/features/features.html'
+})
+
+export class Features {
+
+}
