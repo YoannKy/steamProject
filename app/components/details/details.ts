6156d71349bb488c9267849a907469f2e28d3334
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 18:20:13 +0200

    add components

---
+++ app/components/details/details.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'details'
+})
+@View({
+  templateUrl: 'components/details/details.html'
+})
+
+export class Details {
+
+}
---
+++ app/components/list/list.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'list'
+})
+@View({
+  templateUrl: 'components/list/list.html'
+})
+
+export class List {
+
+}
