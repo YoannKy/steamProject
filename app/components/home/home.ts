d4ff72f50ec05af3072ef6534d959f3bb87481d8
Clement Galidie <galidie.clement@gmail.com>
2015-06-29 16:34:36 +0200

    fix typos

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -4,7 +4,7 @@ import {Component, View} from 'angular2/angular2';
   selector: 'homepage'
 })
 @View({
-  templateUrl: 'components/home/home.html',
+  templateUrl: 'components/home/home.html'
 })
 
 export class Home {
