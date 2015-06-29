a2766b15eeefa452e41832c899162e64bf2cb15e
yoannK <y.keoxay@gmail.com>
2015-06-29 17:44:27 +0200

    autocomplete+ push force (broken repo)

---
+++ app/services/steamService.ts
@@ -0,0 +1,35 @@
+export class SteamService {
+  regex : RegExp; 
+  promise: Promise;
+  games: Array<Object> = []; 
+
+
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      fetch('data/data.json')
+      .then(response => response.json())
+      .then(response => {
+        resolve(response); // resolve promise with response if it fetch succeded
+        }).catch(() => {
+        reject(); // reject promise if we catch a fetch error
+      });
+    });
+    this.promise.then(response => {
+      this.results = response;
+    });  
+  }
+
+  search(search: string, offset: number, limit: number) {
+    this.games = [];
+    this.regex = new RegExp(search,"i");
+    for (var i = 0; i <= this.results.length - 1; i++) {
+      if (this.regex.test(this.results[i].name)) {
+        if (this.games.length <= limit) {
+          this.games.push(this.results[i]);
+        }
+      }
+    };
+    return this.games;
+  }
+
+}
